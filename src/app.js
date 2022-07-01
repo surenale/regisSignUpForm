40e1952235f04ae68346ec04c2dd9e1a4a6e4d59
surenGit <surenale69@gmail.com>
2022-07-01 11:14:47 +0545

    Front-end of login and register

--- src/app.js
+++ src/app.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const { template } = require('handlebars');
 const path = require('path');
+const hbs = require('hbs');
 const app = express();
 
 // requiring db connection to main express app
 require('./db/conn');
 
 const port = process.env.PORT || 3000
-const static_path = path.join(__dirname, "../public");
-app.use(express.static(static_path));
 
+// tell app.js to use static html file 
+const template_path = path.join(__dirname, "../templates/views");
+const partial_path = path.join(__dirname, "../templates/partials");
+
+//set handlebars to main express app
+app.set("view engine", "hbs");
+app.set("views",template_path);
+hbs.registerPartials(partial_path);
+
+// make index.hbs as main home page
 app.get("/", (req, res) => {
-    res.send("Hello it's me Suren Ale");
+    res.render("index");
 })
+
+app.get("/register", (req,res) => {
+    res.render("register");
+})
+
+
+app.get("/login", (req,res) => {
+    res.render("login");
+})
+
 
 app.listen(port, () => {
     console.log(`listening to port at ${port}`);
