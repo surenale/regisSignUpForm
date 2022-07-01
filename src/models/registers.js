1a22d8e23f1478b101ee30261495cef29ef3422f
surenGit <surenale69@gmail.com>
2022-07-01 16:59:20 +0545

    secure password before saving to database using bcryptjs and hash method

--- src/models/registers.js
+++ src/models/registers.js
@@ -1,37 +1,50 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const employeeSchema = new mongoose.Schema({
-     firstname : {
-        type : String,
-        required : true
-     },
-     lastname : {
-        type : String,
-        required : true
-     }, 
-     email : {
-        type : String,
-        required : true,
-        unique : true
-     },
-     phone : {
-        type : String,
-        required : true,
-        unique : true
-     },
-     password : {
-        type : String,
-        required : true,
-     },
-     confirmpassword : {
-        type : String,
-        required : true,
-    },
-    gender : {
-        type : String,
-        required : true
-     }
+   firstname: {
+      type: String,
+      required: true
+   },
+   lastname: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true
+   },
+   phone: {
+      type: String,
+      required: true,
+      unique: true
+   },
+   password: {
+      type: String,
+      required: true,
+   },
+   confirmpassword: {
+      type: String,
+      required: true,
+   },
+   gender: {
+      type: String,
+      required: true
+   }
+})
+
+// secure password with bcryptjs before save to database 
+
+employeeSchema.pre("save", async function (next) {
+   if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 10);
+      console.log(`the hash password is ${this.password}`);
+      this.confirmpassword = undefined;
+
+   }
+   next();
 })
 
 //now we need to create collections
-const Register = new mongoose.model("Register",employeeSchema);
+const Register = new mongoose.model("Register", employeeSchema);
 module.exports = Register;
