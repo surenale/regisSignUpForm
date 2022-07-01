bc91b71615a54a822dcf010f6c61e7b16f664f66
surenGit <surenale69@gmail.com>
2022-07-01 17:28:19 +0545

    login using bcryptjs in password hash

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const bcrypt = require('bcryptjs');
 const app = express();
 
 // requiring db connection to main express app
@@ -66,7 +67,8 @@ app.post("/login", async (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
         const useremail = await Register.findOne({ email: email });
-        if (useremail.password === password) {
+        const isMatch = await bcrypt.compare(password, useremail.password);
+        if (isMatch) {
             res.status(201).render("index");
         }
         else {
